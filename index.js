890084e7b97f36fef102111c77606d5d74561856
Chris Angelico <rosuav@gmail.com>
2017-07-04 00:25:01 +1000

    JS: Introduce proper handling of incoming events

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const events = {
+	StreamStatus: data => {
+		console.log("Stream status");
+	},
+};
+
 function setup()
 {
 	console.log("Initializing");
@@ -6,6 +12,15 @@ function setup()
 	//const announce_pos = (n) => socket.send(JSON.stringify({type: "setpos", data: n}))
 	socket.onmessage = (ev) => {
 		const data = JSON.parse(ev.data);
-		console.log("Packet received and decoded:", data); //PRAD - https://www.theregister.co.uk/2002/02/18/the_bastard_guide_to_writing/
+		if (data["update-type"])
+		{
+			const func = events[data["update-type"]];
+			if (func) return func(data);
+			//Unknown events get logged once and then no more.
+			console.log("Unknown event:", data["update-type"]);
+			events[data["update-type"]] = () => {};
+			return;
+		}
+		console.log("Unknown packet:", data);
 	};
 }
