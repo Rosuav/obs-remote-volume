65ea953c78e0e1a1a71709a623d4a86570965766
Chris Angelico <rosuav@gmail.com>
2019-06-19 16:42:27 +1000

    pingcam: Retry connection failures

--- pingcam.py
+++ pingcam.py
@@ -1,6 +1,7 @@
 # Ping the webcam so it fixes its delay
 # Requires obs-websocket v4.3.0 or newer.
 import json
+import time
 
 # TODO: Reimplement the essential parts (mainly the handshake) and
 # drop the rest. This is a way bigger import than we need.
@@ -10,7 +11,12 @@ import websocket # ImportError? Try: pip install websocket-client
 server = "localhost"
 password = ""
 
-ws = websocket.create_connection("ws://" + server + ":4444/")
+while True:
+	try:
+		ws = websocket.create_connection("ws://" + server + ":4444/")
+		break
+	except ConnectionRefusedError:
+		time.sleep(2) # Retry till OBS is fully up
 data = {"request-type": "SetSourceSettings", "message-id": "fixcam",
 	"sourceName": "Webcam", "sourceSettings": {}}
 ws.send(json.dumps(data))
