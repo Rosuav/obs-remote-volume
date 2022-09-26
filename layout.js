dcdea3731cf07ff1a78253a001c678c854ee5cbf
Chris Angelico <rosuav@gmail.com>
2022-09-26 10:18:55 +1000

    Create the toolbox popup

--- layout.js
+++ layout.js
@@ -1,8 +1,15 @@
 import {choc, DOM, set_content} from "https://rosuav.github.io/choc/factory.js";
-const {P} = choc; //autoimport
+const {LI, LINK, P, SECTION, UL} = choc; //autoimport
 
 DOM("#layoutmode").onclick = () => {
 	const win = window.open("", "toolbox", "popup=1,width=300,height=650");
 	console.log(win)
-	set_content(win.document.body, P("Hello, world!"));
+	set_content(win.document.body, [
+		LINK({rel: "stylesheet", href: new URL("layout.css", location).href}),
+		P("Hello, world!"),
+		SECTION({id: "demo"}, [
+			P("Drag this thing!"),
+			UL("Test list with a number of elements".split(" ").map(w => LI(w))),
+		]),
+	]).id = "toolbox";
 };
