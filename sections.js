741ee2175ca77bdff5f23c39ff58cd3d79eeb471
Chris Angelico <rosuav@gmail.com>
2022-10-03 21:18:52 +1100

    Add title bars to ALL (well, nearly all) elements when editing

--- sections.js
+++ sections.js
@@ -14,13 +14,20 @@ const sections = {
 	],
 };
 
+const typename = {
+	split: "Split bar", iframe: "Embedded Web Page",
+	demo1: "Demo With List",
+	demo2: "Hey Look, A Thing",
+	demo3: "Another Thing",
+};
+
 export const rendered_layout = [];
 console.log(rendered_layout)
 
 let editmode = false;
 function build(layout, parent, self) {
-	console.log("Build", layout);
-	let ret = null;
+	let ret = null, tb = editmode, drag = editmode;
+	let deftitle = null;
 	const layoutidx = rendered_layout.length;
 	rendered_layout.push(layout);
 	switch (layout.type) {
@@ -28,6 +35,7 @@ function build(layout, parent, self) {
 		case "box": ret = DIV({
 			class: "box " + (layout.orientation === "vertical" ? "vertical" : "horizontal"),
 		}, layout.children.map((l,i) => build(l, layoutidx, i)));
+		tb = drag = false;
 		break;
 		//A splitbar has precisely two children (either or both of which can be null),
 		//and renders them as left+right or top+bottom depending on orientation.
@@ -37,7 +45,7 @@ function build(layout, parent, self) {
 			children[0].style[layout.orientation === "vertical" ? "height" : "width"] =
 				typeof layout.splitpos === "number" ? layout.splitpos + "px" : "50%";
 			ret = DIV({
-				class: "draggable split " + (layout.orientation === "vertical" ? "vertical" : "horizontal"),
+				class: "split " + (layout.orientation === "vertical" ? "vertical" : "horizontal"),
 			}, [
 				children[0],
 				DIV({class: "splitbar"}),
@@ -45,25 +53,32 @@ function build(layout, parent, self) {
 			]);
 			break;
 		}
-		//A section provides a standard element.
-		case "section": ret = SECTION({id: layout.id, class: "draggable droptarget"}, sections[layout.id](layout)); break;
-		case "master": ret = DIV(build(layout.children[0], layoutidx, 0)); break;
-		case "shadow": ret = DIV({class: "shadow droptarget"}); break;
+		case "section":
+			ret = SECTION({id: layout.id, class: "droptarget"}, sections[layout.id](layout));
+			deftitle = typename[layout.id] || "Section";
+			break;
+		case "master": ret = DIV(build(layout.children[0], layoutidx, 0)); tb = drag = false; break;
+		case "shadow": ret = DIV({class: "shadow droptarget"}); tb = drag = false; break;
 		case "iframe":
-			if (layout.titlebar || 1) { //Or if we're in edit mode - always have title bar while editing
-				ret = DIV({class: "box vertical draggable"}, [
-					DIV({class: "titlebar"}, layout.title || layout.src || "Embedded Web Page"),
-					IFRAME({src: layout.src || "iframedemo.html"}),
-				]);
-			}
-			else ret = IFRAME({src: layout.src || "iframedemo.html", class: "draggable"});
+			//TODO: Reuse the iframe where possible. It may need to have an ID assigned.
+			ret = IFRAME({src: layout.src || "iframedemo.html"});
+			if (layout.titlebar) {tb = true; deftitle = ret.contentDocument.title;}
 			break;
 		default: break;
 	}
-	if (!ret) ret = DIV({class: "droptarget", style: "width: 100%; height: 100%"}); //Empty slot in a split or master
+	if (!ret) {ret = DIV({class: "droptarget", style: "width: 100%; height: 100%"}); tb = drag = false;} //Empty slot in a split or master
 	ret.dataset.parentidx = parent;
 	ret.dataset.selfidx = self;
-	if (editmode && ret.classList.contains("draggable")) {
+	if (tb) { //Some elements have titlebars in edit mode. It's possible for them to have them in layout mode too.
+		ret = DIV({class: "box vertical"}, [
+			DIV({class: "titlebar"}, layout.title || deftitle || typename[layout.type] || layout.type || "Element"),
+			ret,
+		]);
+		ret.dataset.parentidx = parent;
+		ret.dataset.selfidx = self;
+	}
+	if (drag) { //Items are only ever draggable during edit mode. Some still aren't, even then.
+		ret.classList.add("draggable");
 		ret.draggable = true;
 		ret.dataset.draglayout = JSON.stringify(layout);
 	}
